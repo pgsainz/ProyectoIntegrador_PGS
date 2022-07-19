5de0d4abb2f7c84f6f0316fdd5a6748f0ecc2504
Pablo G. Sainz <pgsainz@hotmail.com>
2022-07-19 01:56:09 -0300

    ajustes 001

--- ProyectoIntegrador_PGS/src/app/app.module.ts
+++ ProyectoIntegrador_PGS/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoArgPComponent } from './components/logo-arg-p/logo-arg-p.component';
+import { SocialesComponent } from './components/sociales/sociales.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoArgPComponent,
+    SocialesComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
