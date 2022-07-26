f9951d28a2061b6fda73bc6a9c2856fc7d18a2dc
pgsainz <pgsainz@gmail.com>
2022-07-26 01:07:40 -0300

    Refresco Total

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { AcercaComponent }  from './components/acerca/acerca.component';
 import { LogoArgPComponent } from './components/logo-arg-p/logo-arg-p.component';
 import { SocialesComponent } from './components/sociales/sociales.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    AcercaComponent,
     LogoArgPComponent,
     SocialesComponent,
-    BannerComponent
+    BannerComponent,
+    EducacionComponent,
+    ExperienciaComponent
   ],
-  imports: [
+  imports:[
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({}),
   ],
   providers: [],
   bootstrap: [AppComponent]
